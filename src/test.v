e04f06839fb82fad3164a4a58b1ee066cbd9cecb
Chenghui Weng <144981080+weng-chenghui@users.noreply.github.com>
2025-09-10 11:23:02 +0900

    Prepare CI

--- src/test.v
+++
@@ -1,8 +0,0 @@
-From mathcomp Require Import ssreflect ssrfun ssrbool eqtype.
-Set Implicit Arguments.
-Unset Strict Implicit.
-Unset Printing Implicit Defensive.
-Section S.
-Lemma idP_true : true = true.
-Proof. by []. Qed.
-End S.
